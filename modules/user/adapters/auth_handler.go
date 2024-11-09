e98cede53c2fdc47c133750818577ef8c2cbf0d8
phetploy <st.phetploy@gmail.com>
2024-11-09 22:12:51 +0700

    :recycle: refactor register handler for testability

--- modules/user/adapters/auth_handler.go
+++ modules/user/adapters/auth_handler.go
@@ -1,29 +1,54 @@
 package adapters
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"github.com/phetployst/art-toys-store/modules/user/entities"
-	"github.com/phetployst/art-toys-store/pkg/request"
-	"github.com/phetployst/art-toys-store/pkg/response"
 )
 
-func (h *httpUserHandler) Register(c echo.Context) error {
-	var user entities.User
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
 
-	wrapper := request.ContextWrapper(c)
-	if err := wrapper.Bind(&user); err != nil { // bind and validate
-		return response.ErrorResponse(c, http.StatusBadRequest, err.Error())
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (c *CustomValidator) Validate(i interface{}) error {
+	if err := c.validator.Struct(i); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (h *httpUserHandler) Register(c echo.Context) error {
+	user := new(entities.User)
+
+	validator := validator.New()
+	c.Echo().Validator = &CustomValidator{validator: validator}
+
+	if err := c.Bind(&user); err != nil {
+		log.Printf("failed to bind input %v", err)
+		return c.JSON(http.StatusBadRequest, ErrorResponse{Message: "Invalid request data"})
 	}
 
-	userAccount, err := h.usecase.CreateNewUser(&user)
+	if err := c.Validate(user); err != nil {
+		log.Printf("failed to validate input %v", err)
+		return c.JSON(http.StatusBadRequest, ErrorResponse{Message: "request data validation failed"})
+	}
+
+	userAccount, err := h.usecase.CreateNewUser(user)
 	if err != nil {
 		if err.Error() == "email or username already exists" {
-			return response.ErrorResponse(c, http.StatusConflict, err.Error())
+			return c.JSON(http.StatusConflict, err.Error())
 		}
-		return response.ErrorResponse(c, http.StatusInternalServerError, err.Error())
+
+		log.Printf("failed to create new user: %v", err)
+		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
 
-	return response.SuccessResponse(c, http.StatusCreated, userAccount)
+	return c.JSON(http.StatusCreated, userAccount)
 }
--- modules/user/adapters/auth_handler_test.go
+++ modules/user/adapters/auth_handler_test.go
@@ -13,23 +13,9 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-type MockUserUsecase struct {
-	mock.Mock
-}
-
-func (m *MockUserUsecase) CreateNewUser(user *entities.User) (*entities.UserAccount, error) {
-	args := m.Called(user)
-	return nil, args.Error(0)
-}
-
-func (m *MockUserUsecase) UserAccountById(userId uint) (*entities.UserAccount, error) {
-	args := m.Called(userId)
-	return nil, args.Error(0)
-}
-
 func TestRegisterHandler_auth(t *testing.T) {
 
-	t.Run("given successful registration", func(t *testing.T) {
+	t.Run("register given valid user should be successful", func(t *testing.T) {
 		mockService := new(MockUserUsecase)
 		handler := &httpUserHandler{usecase: mockService}
 
@@ -50,7 +36,43 @@ func TestRegisterHandler_auth(t *testing.T) {
 		assert.Equal(t, http.StatusCreated, response.Code)
 	})
 
-	t.Run("given email or username already exists", func(t *testing.T) {
+	t.Run("register given error during user binding", func(t *testing.T) {
+		mockService := new(MockUserUsecase)
+		handler := &httpUserHandler{usecase: mockService}
+
+		e := echo.New()
+		defer e.Close()
+
+		request := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(`{1234}`))
+		request.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		response := httptest.NewRecorder()
+		c := e.NewContext(request, response)
+
+		err := handler.Register(c)
+
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusBadRequest, response.Code)
+	})
+
+	t.Run("register given invalid user", func(t *testing.T) {
+		mockService := new(MockUserUsecase)
+		handler := &httpUserHandler{usecase: mockService}
+
+		e := echo.New()
+		defer e.Close()
+
+		request := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(`{"username": "phetploy"}`))
+		request.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		response := httptest.NewRecorder()
+		c := e.NewContext(request, response)
+
+		err := handler.Register(c)
+
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusBadRequest, response.Code)
+	})
+
+	t.Run("register given error email or username already exists", func(t *testing.T) {
 		mockService := new(MockUserUsecase)
 		handler := &httpUserHandler{usecase: mockService}
 
@@ -72,7 +94,7 @@ func TestRegisterHandler_auth(t *testing.T) {
 
 	})
 
-	t.Run("given internal server error", func(t *testing.T) {
+	t.Run("register given create user usecase internal server error", func(t *testing.T) {
 		mockService := new(MockUserUsecase)
 		handler := &httpUserHandler{usecase: mockService}
 
@@ -93,4 +115,13 @@ func TestRegisterHandler_auth(t *testing.T) {
 		assert.Equal(t, http.StatusInternalServerError, response.Code)
 	})
 
+}
+
+type MockUserUsecase struct {
+	mock.Mock
+}
+
+func (m *MockUserUsecase) CreateNewUser(user *entities.User) (*entities.UserAccount, error) {
+	args := m.Called(user)
+	return nil, args.Error(0)
 }
